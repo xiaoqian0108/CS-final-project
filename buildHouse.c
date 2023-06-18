08d318198cfa661a93c6fc3bb8583fe6fe9cb176
xiaoqian0108 <113163797+xiaoqian0108@users.noreply.github.com>
2023-06-18 18:24:43 +0800

    Update buildHouse.c

--- buildHouse.c
+++ buildHouse.c
@@ -4,7 +4,7 @@
 #include "build.h"
 #include "time.h"
 
-void build_house( int32_t player, int32_t house, Plate *ArrayPlate, Vertex *ArrayVertex, Line *ArrayLine, Player *ArrayPlayer ) //1:village, 2:city
+void build_house( int32_t player, int32_t init, int32_t house, Plate *ArrayPlate, Vertex *ArrayVertex, Line *ArrayLine, Player *ArrayPlayer ) //1:village, 2:city
 {
 	int32_t vertex_neighbor[54][3] = { 
 	{0, 1, 0},
@@ -121,110 +121,333 @@ void build_house( int32_t player, int32_t house, Plate *ArrayPlate, Vertex *Arra
 		line_neighbor[i][1] = i / 2 + 18;
 	}
 
-	if( house == 1 )
+
+
+
+	if( init == 1 )
 	{
-		if( ArrayPlayer[player - 1].resource[4] == 0 || ArrayPlayer[player - 1].resource[1] == 0 || ArrayPlayer[player - 1].resource[0] == 0 || ArrayPlayer[player - 1].resource[2] == 0 )
+		if( player == 0)
 		{
-			printf("Your resources are insufficient! \n");
-			return;
-		}
-	}
+			int32_t buildvillage_num = 0;
+			int32_t buildroad_num = 0;
+			int32_t NotBulid_init = 0;
 
-	if( house == 1 )
-	{
-		int32_t buildvillage_num = 0;
-
-		init_map( ArrayPlate );
-		map( ArrayPlate, ArrayVertex, ArrayLine );
-		
-		int32_t test = 0;
-		int32_t NotBulid = 0;
-
-		while( NotBulid == 0 )
-		{
-			printf("Which one do you want to build village ? \n");
-			printf("Please enter: ");
-			scanf("%d", &buildvillage_num);
-
-			if( vertex_neighbor[buildvillage_num][2] == 0 )
-			{	
-				if( ArrayLine[ vertex_neighbor[buildvillage_num][0] ].road != player && ArrayLine[ vertex_neighbor[buildvillage_num][1] ].road != player )
-				{
-					printf("This point cannot be covered !\n");
-					continue;
-				}
-			}
-			else
+			while( NotBulid_init == 0 )
 			{
-				if( ArrayLine[ vertex_neighbor[buildvillage_num][0] ].road != player && ArrayLine[ vertex_neighbor[buildvillage_num][1] ].road != player 
-					&& ArrayLine[ vertex_neighbor[buildvillage_num][2] ].road != player )
-				{
-					printf("This point cannot be covered !\n");
-					continue;
-				}
-			}
-			
+				init_map( ArrayPlate );
+				map( ArrayPlate, ArrayVertex, ArrayLine );
+				printf("Which one do you want to build village ? \n");
+				printf("Please enter: ");
+				scanf("%d", &buildvillage_num);
 
+				for(int32_t i = 0; i < 3; i++)
+				{
+					if( i == 2 && vertex_neighbor[buildvillage_num][i] == 0)
+					{
+						continue;
+					}
+					else
+					{
+						int32_t adjacentline = vertex_neighbor[buildvillage_num][i];
+						int32_t test = (line_neighbor[adjacentline][0] != buildvillage_num) ? line_neighbor[adjacentline][0] : buildvillage_num;
+						if( ArrayVertex[test].village != 0 || ArrayVertex[test].city != 0 )
+						{
+							NotBulid_init = 1;
+						}
+					}
+				}
+				if( NotBulid_init == 1 || ArrayVertex[buildvillage_num].village != 0 || ArrayVertex[buildvillage_num].city != 0 )
+				{
+					NotBulid_init = 0;
+					printf("This point cannot be covered !!!\n");
+				}
+				else
+				NotBulid_init = 2;
+			}
+	
+			ArrayVertex[buildvillage_num].village = 1;
+
+			printf("Which one do you want to build road ? \n");
+			printf("You can only choose ");
 			for(int32_t i = 0; i < 3; i++)
 			{
-				if( i == 2 && vertex_neighbor[buildvillage_num][i] == 0)
+				if(i == 2 && vertex_neighbor[buildvillage_num][2] == 0)
 				{
 					continue;
 				}
 				else
 				{
-					int32_t adjacentline = vertex_neighbor[buildvillage_num][i];
-					test = (line_neighbor[adjacentline][0] != buildvillage_num) ? line_neighbor[adjacentline][0] : buildvillage_num;
-					if( ArrayVertex[test].village != 0 || ArrayVertex[test].city != 0 )
+					printf("%d ", vertex_neighbor[buildvillage_num][i]);
+				}
+			}
+			printf("\n");
+			printf("Please enter: ");
+			scanf("%d", &buildroad_num);
+			ArrayLine[buildroad_num].road = 1;
+		}
+		else if( player == 1 || player == 2 || player == 3 )
+		{
+			srand( time(0) );
+			int32_t site = 0;
+
+			do 
+			{
+		    	site = rand() % 54;
+			} 
+			while (ArrayVertex[site].village != 0);
+
+			ArrayVertex[site].village = player + 1;
+
+			int32_t siteRoad = 0;
+			if( vertex_neighbor[site][2] == 0 )
+			{
+				siteRoad = rand() % 2;
+			}
+			else
+			{
+				siteRoad = rand() % 3;
+			}
+			int32_t road = vertex_neighbor[site][siteRoad];
+			ArrayLine[road].road = player + 1;
+		}
+	}
+	else
+	{
+		if( player == 0 )
+		{
+			if( house == 1 )
+			{
+				if( ArrayPlayer[player].resource[4] == 0 || ArrayPlayer[player].resource[1] == 0 || ArrayPlayer[player].resource[0] == 0 || ArrayPlayer[player].resource[2] == 0 )
+				{
+					printf("Your resources are insufficient! \n");
+					return;
+				}
+			}
+
+			if( house == 1 )
+			{
+				int32_t buildvillage_num = 0;
+
+				init_map( ArrayPlate );
+				map( ArrayPlate, ArrayVertex, ArrayLine );
+				
+				int32_t test = 0;
+				int32_t NotBulid = 0;
+
+				while( NotBulid == 0 )
+				{
+					printf("Which one do you want to build village ? \n");
+					printf("Please enter: ");
+					scanf("%d", &buildvillage_num);
+
+					if( vertex_neighbor[buildvillage_num][2] == 0 )
+					{	
+						if( ArrayLine[ vertex_neighbor[buildvillage_num][0] ].road != player + 1 && ArrayLine[ vertex_neighbor[buildvillage_num][1] ].road != player + 1 )
+						{
+							printf("This point cannot be covered ?!\n");
+							continue;
+						}
+					}
+					else
 					{
-						NotBulid = 1;
+						if( ArrayLine[ vertex_neighbor[buildvillage_num][0] ].road != player + 1 && ArrayLine[ vertex_neighbor[buildvillage_num][1] ].road != player + 1
+							&& ArrayLine[ vertex_neighbor[buildvillage_num][2] ].road != player + 1 )
+						{
+							printf("This point cannot be covered !!\n");
+							continue;
+						}
+					}
+
+
+					for(int32_t i = 0; i < 3; i++)
+					{
+						if( i == 2 && vertex_neighbor[buildvillage_num][i] == 0)
+						{
+							continue;
+						}
+						else
+						{
+							int32_t adjacentline = vertex_neighbor[buildvillage_num][i];
+							test = (line_neighbor[adjacentline][0] != buildvillage_num) ? line_neighbor[adjacentline][0] : buildvillage_num;
+							if( ArrayVertex[test].village != 0 || ArrayVertex[test].city != 0 )
+							{
+								NotBulid = 1;
+							}
+						}
+					}
+					if( NotBulid == 1)
+					{
+						NotBulid = 0;
+						printf("This point cannot be covered !!!\n");
+					}
+					else
+					NotBulid = 2;
+				}
+
+				ArrayVertex[ buildvillage_num ].village == player + 1;
+				ArrayPlayer[player - 1].resource[4]--;
+				ArrayPlayer[player - 1].resource[1]--;
+				ArrayPlayer[player - 1].resource[0]--;
+				ArrayPlayer[player - 1].resource[2]--;
+				ArrayPlayer[player - 1].building[1]++;
+			}
+
+
+			if( house == 2 )
+			{
+				if( ArrayPlayer[player].resource[3] < 3 || ArrayPlayer[player].resource[0] < 2 )
+				{
+					printf("Your resources are insufficient! \n");
+					return;
+				}
+			}
+			if( house == 2 )
+			{
+				int32_t buildcity_num = 0;
+				int32_t OK = 0;
+
+				while( OK == 0 )
+				{
+					init_map( ArrayPlate );
+					map( ArrayPlate, ArrayVertex, ArrayLine );
+					printf("Which one do you want to build city ? \n");
+					printf("Please enter: ");
+					scanf("%d", &buildcity_num);
+
+					if( ArrayVertex[buildcity_num].village == player + 1 )
+					{
+						ArrayVertex[buildcity_num].village = 0;
+						ArrayVertex[buildcity_num].city = player + 1;
+
+						ArrayPlayer[player].resource[3] = ArrayPlayer[player].resource[3] - 3;
+						ArrayPlayer[player].resource[0] = ArrayPlayer[player].resource[0] - 2;
+						ArrayPlayer[player].building[2]++;
+						OK = 1;
+					}
+					else
+					{
+						printf("This point cannot be covered !\n");
 					}
 				}
 			}
-			if( NotBulid == 1)
-			{
-				NotBulid = 0;
-				printf("This point cannot be covered !\n");
-			}
-			else
-			NotBulid = 2;
 		}
 
-		ArrayVertex[ buildvillage_num ].village == player;
-		ArrayPlayer[player - 1].resource[4]--;
-		ArrayPlayer[player - 1].resource[1]--;
-		ArrayPlayer[player - 1].resource[0]--;
-		ArrayPlayer[player - 1].resource[2]--;
-		ArrayPlayer[player - 1].building[1]++;
-	}
-
-
-	if( house == 2 )
-	{
-		if( ArrayPlayer[player - 1].resource[3] < 3 || ArrayPlayer[player - 1].resource[0] < 2 )
+		if( player == 1 || player == 2 || player == 3 )
 		{
-			printf("Your resources are insufficient! \n");
-			return;
+			if( house == 1 )
+			{
+				srand( time(0) );
+				int32_t site = 0;
+				int32_t choose_ver1 = 0;
+				int32_t choose_ver2 = 0;
+				int32_t ver = 0;
+
+				while( choose_ver1 == 0 && choose_ver2 == 0 )
+				{
+					do 
+					{
+						site = rand() % 72;
+					} 
+					while (ArrayLine[site].road != player + 1);
+
+					
+					for(int32_t i = 0; i < 2; i++)
+					{
+						if( vertex_neighbor[ line_neighbor[site][i] ][2] == 0 )
+						{
+							for(int32_t j = 0; j < 2; j++)
+							{
+								int32_t line = vertex_neighbor[ line_neighbor[site][i] ][j];
+								int32_t vertex = ( line_neighbor[line][0] != line_neighbor[site][i] ) ? line_neighbor[line][0] : line_neighbor[line][1];
+								
+								if( ArrayVertex[vertex].village == 0 && ArrayVertex[vertex].city == 0 )
+								{
+									if( i == 0 )
+									{
+										choose_ver1++; // 2代表這個點沒有東西
+									}
+									else if( i == 1 )
+									{
+										choose_ver2++;
+									}
+								}
+							}
+
+							if( choose_ver1 == 2 )
+							{
+								ver = line_neighbor[site][0];
+							}
+							else if( choose_ver2 == 2 )
+							{
+								ver = line_neighbor[site][1];
+							}
+						}
+						else
+						{
+							for(int32_t j = 0; j < 3; j++)
+							{
+								int32_t line = vertex_neighbor[ line_neighbor[site][i] ][j];
+								int32_t vertex = ( line_neighbor[line][0] != line_neighbor[site][i] ) ? line_neighbor[line][0] : line_neighbor[line][1];
+								
+								if( ArrayVertex[vertex].village == 0 && ArrayVertex[vertex].city == 0 )
+								{
+									if( i == 0 )
+									{
+										choose_ver1++; // 3代表這個點沒有東西
+									}
+									else if( i == 1 )
+									{
+										choose_ver2++;
+									}
+								}
+							}
+							if( choose_ver1 == 3 )
+							{
+								ver = line_neighbor[site][0];
+							}
+							else if( choose_ver2 == 3 )
+							{
+								ver = line_neighbor[site][1];
+							}
+							
+						}
+					}
+				}
+
+				ArrayVertex[ver].village = player + 1;
+			}
+
+			int32_t citysite = 0;
+			if( house == 2 )
+			{
+				do 
+				{
+					citysite = rand() % 54;
+				} 
+				while (ArrayVertex[citysite].village != player + 1);
+
+				ArrayVertex[citysite].city = player + 1;
+				ArrayVertex[citysite].village = 0;
+
+				ArrayPlayer[player].resource[3] = ArrayPlayer[player].resource[3] - 3;
+				ArrayPlayer[player].resource[0] = ArrayPlayer[player].resource[0] - 2;
+				ArrayPlayer[player].building[2]++;
+			}
 		}
 	}
-	if( house == 2 )
-	{
-		int32_t buildcity_num = 0;
-
-		init_map( ArrayPlate );
-		map( ArrayPlate, ArrayVertex, ArrayLine );
-		printf("Which one do you want to build city ? \n");
-		printf("Please enter: ");
-		scanf("%d", &buildcity_num);
-
-		ArrayVertex[buildcity_num].village = 0;
-		ArrayVertex[buildcity_num].city = player;
-
-		ArrayPlayer[player - 1].resource[3] = ArrayPlayer[player - 1].resource[3] - 3;
-		ArrayPlayer[player - 1].resource[0] = ArrayPlayer[player - 1].resource[0] - 2;
-		ArrayPlayer[player - 1].building[2]++;
-	}
-
 
 	return;
 }
+
+/*
+    主函式要寫的大家剛開始蓋兩棟村莊，兩條路
+    build_house( 0, 1, 0, ArrayPlate, ArrayVertex, ArrayLine, ArrayPlayer );
+	build_house( 1, 1, 0, ArrayPlate, ArrayVertex, ArrayLine, ArrayPlayer );
+	build_house( 2, 1, 0, ArrayPlate, ArrayVertex, ArrayLine, ArrayPlayer );
+	build_house( 3, 1, 0, ArrayPlate, ArrayVertex, ArrayLine, ArrayPlayer );
+	
+	build_house( 3, 1, 0, ArrayPlate, ArrayVertex, ArrayLine, ArrayPlayer );
+	build_house( 2, 1, 0, ArrayPlate, ArrayVertex, ArrayLine, ArrayPlayer );
+	build_house( 1, 1, 0, ArrayPlate, ArrayVertex, ArrayLine, ArrayPlayer );
+	build_house( 0, 1, 0, ArrayPlate, ArrayVertex, ArrayLine, ArrayPlayer );
+*/
