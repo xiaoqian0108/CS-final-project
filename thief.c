a453e391640fd147ac4232c3032b3d3f427b2528
yum404 <136248607+yum404@users.noreply.github.com>
2023-06-18 12:28:11 +0800

    Add files via upload

--- thief.c
+++ thief.c
@@ -10,11 +10,13 @@ int isthief( Vertex *pVertex, Player *pPlayer, int32_t ary[], int32_t resource_N
 	
     int32_t p[4] = {0}, amount = 0, oneplayer, resource;
     for(i = 0; i < 6; i++)
-    {
+    { 
     	if( pVertex[ary[i]].village > 1)
 	    {
+	    	//Record which player's village (or city) is on the plate
 	        p[amount] = pVertex[i].village;
 	        p[amount]--;
+	        //Record how many players on the plate
 	        amount++;
 	    }
     }
@@ -29,8 +31,11 @@ int isthief( Vertex *pVertex, Player *pPlayer, int32_t ary[], int32_t resource_N
     {
     	while(1)
     	{
+    		//Randomly select the resource
     		srand(time(NULL));
     		int resource_NO = rand() % 5 + 1;
+
+
     		if(pPlayer[p[0]].resource[resource_NO - 1] > 0)
     		{
     			pPlayer[p[0]].resource[resource_NO - 1]--;
@@ -201,115 +206,115 @@ void thief(Plate *pPlate, Vertex *pVertex, Player *pPlayer)
 
 		if(point == 1 && point != recent)
 		{
-			int ary[] = {0, 3, 4, 7, 8, 12};
+			int ary[6] = {0, 3, 4, 7, 8, 12};
 	        a = isthief( pVertex, pPlayer, ary, pPlate[0].scene, trc);
 	        pPlate[0].bandit = 1;
 		}
 		else if(point == 2 && point != recent)
 		{
-	        int ary[] = {1, 4, 5, 8, 9, 13};
+	        int ary[6] = {1, 4, 5, 8, 9, 13};
 	        a = isthief( pVertex, pPlayer, ary, pPlate[1].scene, trc);
 	        pPlate[1].bandit = 1;
 	    }
 	    else if(point == 3 && point != recent)
 	    {
-	    	int ary[] = {2, 5, 6, 9, 10, 14};
+	    	int ary[6] = {2, 5, 6, 9, 10, 14};
 	        a = isthief( pVertex, pPlayer, ary, pPlate[2].scene, trc);
 	        pPlate[2].bandit = 1;
 	    }
 	    else if(point == 4 && point != recent)
 	    {
-	    	int ary[] = {7, 11, 12, 16, 17, 22};
+	    	int ary[6] = {7, 11, 12, 16, 17, 22};
 	        a = isthief( pVertex, pPlayer, ary, pPlate[3].scene, trc);
 	        pPlate[3].bandit = 1;
 	    }
 	    else if(point == 5 && point != recent)
 	    {
-	    	int ary[] = {8, 12, 13, 17, 18, 23};
+	    	int ary[6] = {8, 12, 13, 17, 18, 23};
 	        a = isthief( pVertex, pPlayer, ary, pPlate[4].scene, trc);
 	        pPlate[4].bandit = 1;
 	    }
 	    else if(point == 6 && point != recent)
 	    {
-	    	int ary[] = {9, 13, 14, 18, 19, 24};
+	    	int ary[6] = {9, 13, 14, 18, 19, 24};
 	        a = isthief( pVertex, pPlayer, ary, pPlate[5].scene, trc);
 	        pPlate[5].bandit = 1;
 	    }
 	    else if(point == 7 && point != recent)
 	    {
-	    	int ary[] = {10, 14, 15, 19, 20, 25};
+	    	int ary[6] = {10, 14, 15, 19, 20, 25};
 	        a = isthief( pVertex, pPlayer, ary, pPlate[6].scene, trc);
 	        pPlate[6].bandit = 1;
 	    }
 	    else if(point == 8 && point != recent)
 	    {
-	    	int ary[] = {16, 21, 22, 27, 28, 33};
+	    	int ary[6] = {16, 21, 22, 27, 28, 33};
 	        a = isthief( pVertex, pPlayer, ary, pPlate[7].scene, trc);
 	        pPlate[7].bandit = 1;
 	    }
 	    else if(point == 9 && point != recent)
 	    {
-	    	int ary[] = {17, 22, 23, 28, 29, 34};
+	    	int ary[6] = {17, 22, 23, 28, 29, 34};
 	        a = isthief( pVertex, pPlayer, ary, pPlate[8].scene, trc);
 	        pPlate[8].bandit = 1;
 	    }
 	    else if(point == 10 && point != recent)
 	    {
-	    	int ary[] = {18, 23, 24, 29, 30, 35};
+	    	int ary[6] = {18, 23, 24, 29, 30, 35};
 	        a = isthief( pVertex, pPlayer, ary, pPlate[9].scene, trc);
 	        pPlate[9].bandit = 1;
 	    }
 	    else if(point == 11 && point != recent)
 	    {
-	    	int ary[] = {19, 24, 25, 30, 31, 36};
+	    	int ary[6] = {19, 24, 25, 30, 31, 36};
 	        a = isthief( pVertex, pPlayer, ary, pPlate[10].scene, trc);
 	        pPlate[10].bandit = 1;
 	    }
 	    else if(point == 12 && point != recent)
 	    {
-	    	int ary[] = {20, 25, 26, 31, 32, 37};
+	    	int ary[6] = {20, 25, 26, 31, 32, 37};
 	        a = isthief( pVertex, pPlayer, ary, pPlate[11].scene, trc);
 	        pPlate[11].bandit = 1;
 	    }
 	    else if(point == 13 && point != recent)
 	    {
-	    	int ary[] = {28, 33, 34, 38, 39, 43};
+	    	int ary[6] = {28, 33, 34, 38, 39, 43};
 	        a = isthief( pVertex, pPlayer, ary, pPlate[12].scene, trc);
 	        pPlate[12].bandit = 1;
 	    }
 	    else if(point == 14 && point != recent)
 	    {
-	    	int ary[] = {29, 34, 35, 39, 40, 44};
+	    	int ary[6] = {29, 34, 35, 39, 40, 44};
 	        a = isthief( pVertex, pPlayer, ary, pPlate[13].scene, trc);
 	        pPlate[13].bandit = 1;
 	    }
 	    else if(point == 15 && point != recent)
 	    {
-	    	int ary[] = {30, 35, 36, 40, 41, 45};
+	    	int ary[6] = {30, 35, 36, 40, 41, 45};
 	        a = isthief( pVertex, pPlayer, ary, pPlate[14].scene, trc);
 	        pPlate[14].bandit = 1;
 	    }
 	    else if(point == 16 && point != recent)
 	    {
-	    	int ary[] = {31, 36, 37, 41, 42, 46};
+	    	int ary[6] = {31, 36, 37, 41, 42, 46};
 	        a = isthief( pVertex, pPlayer, ary, pPlate[15].scene, trc);
 	        pPlate[15].bandit = 1;
 	    }
 	    else if(point == 17 && point != recent)
 	    {
-	    	int ary[] = {39, 43, 44, 47, 48, 51};
+	    	int ary[6] = {39, 43, 44, 47, 48, 51};
 	        a = isthief( pVertex, pPlayer, ary, pPlate[16].scene, trc);
 	        pPlate[16].bandit = 1;
 	    }
 	    else if(point == 18 && point != recent)
 	    {
-	    	int ary[] = {40, 44, 45, 48, 49, 52};
+	    	int ary[6] = {40, 44, 45, 48, 49, 52};
 	        a = isthief( pVertex, pPlayer, ary, pPlate[17].scene, trc);
 	        pPlate[17].bandit = 1;
 	    }
 	    else if(point == 19 && point != recent)
 	    {
-	    	int ary[] = {41, 45, 46, 49, 50, 53};
+	    	int ary[6] = {41, 45, 46, 49, 50, 53};
 	        a = isthief( pVertex, pPlayer, ary, pPlate[18].scene, trc);
 	        pPlate[18].bandit = 1;
 	    }
